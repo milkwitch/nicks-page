37418b982048f2ce9523cadca10a1fecd63b1bf7
milkwitch <dandelionwalk@gmail.com>
2019-06-05 11:54:15 -0400

    (skeleton) basic additions to boilerplate

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div id='nav-container'>
+      <div id='logo'>LOGO</div>
+      <div id='menu-btn'>MENU</div>
+    </div>
+  )
+}
+
+export default Navbar
