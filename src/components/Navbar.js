fdf9b127468d6e549df46ee277767f8a026e023f
milkwitch <dandelionwalk@gmail.com>
2019-06-05 12:53:36 -0400

    (menu) hamburger menu begun

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,67 @@
 import React from 'react'
+import '../stylesheets/navbar.css'
+import { stack as Menu } from 'react-burger-menu'
 
-const Navbar = () => {
-  return (
+var styles = {
+  bmBurgerButton: {
+    position: 'fixed',
+    width: '36px',
+    height: '30px',
+    left: '36px',
+    top: '36px'
+  },
+  bmBurgerBars: {
+    background: '#373a47'
+  },
+  bmBurgerBarsHover: {
+    background: '#a90000'
+  },
+  bmCrossButton: {
+    height: '24px',
+    width: '24px'
+  },
+  bmCross: {
+    background: '#bdc3c7'
+  },
+  bmMenuWrap: {
+    position: 'fixed',
+    height: '100%'
+  },
+  bmMenu: {
+    background: '#373a47',
+    padding: '2.5em 1.5em 0',
+    fontSize: '1.15em'
+  },
+  bmMorphShape: {
+    fill: '#373a47'
+  },
+  bmItemList: {
+    color: '#b8b7ad',
+    padding: '0.8em'
+  },
+  bmItem: {
+    display: 'inline-block'
+  },
+  bmOverlay: {
+    background: 'rgba(0, 0, 0, 0.3)'
+  }
+}
+
+class Navbar extends React.Component {
+  showSettings (event) {
+    event.preventDefault();
+  }
+  
+  render () {
+    return (
     <div id='nav-container'>
       <div id='logo'>LOGO</div>
-      <div id='menu-btn'>MENU</div>
+      <Menu styles={styles}>
+        <a id='home' className='menu-item' href='/'>Home</a>
+      </Menu>
     </div>
-  )
+    )
+  }
 }
 
 export default Navbar
